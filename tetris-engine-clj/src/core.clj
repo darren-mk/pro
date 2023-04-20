47ab182f0fe5ec8a140fd2545a7edb2398c4b442
Darren Kim <darren.minsoo.kim@gmail.com>
2023-04-20 16:54:36 -0400

    made first working draft

--- tetris-engine-clj/src/core.clj
+++ tetris-engine-clj/src/core.clj
@@ -1,8 +1,17 @@
 (ns core)
 
-(defn pad [v height]
+(def shapes
+  {:q [[1 1] [1 1]]
+   :z [[0 1] [1 1] [1]]
+   :s [[1] [1 1] [0 1]]
+   :t [[0 1] [1 1] [0 1]]
+   :i [[1] [1] [1] [1]]
+   :l [[1 1 1] [1]]
+   :j [[1] [1 1 1]]})
+
+(defn cushion [v height]
   (let [padding (take (- height (count v))
-                      (repeat nil))]
+                      (repeat 0))]
     (-> v (concat padding) vec)))
 
 (defn count-zero-pieces
@@ -18,33 +27,104 @@
         computed (- (count base) beg-zeros)]
     (if (neg-int? computed) 0 computed)))
 
-(defn find-max-height [base shape]
+(defn find-required-height [base shape]
   (apply max (map find-height-single base shape)))
 
-#_#_#_
-[:i 0]
-[:i 4]
-[:q 8]
-
-(def t
-  [[0 1]
-   [1 1]
-   [0 1]])
-
-(def panel
-  (atom (into [] (take 10 (iterate identity [])))))
+(defn init-panel []
+  (vec (take 10 (iterate identity []))))
 
 (defn fill [base col height]
-  (concat (pad base height) col))
+  (if (empty? col)
+    base
+    (vec (reduce
+          (fn [b item] (let [v (val item)]
+                         (if (zero? v) b
+                             (assoc b (+ height (key item)) (val item)))))
+          (cushion base (+ height (count col)))
+          (zipmap (range) col)))))
 
-(defn land! [shape col]
+(defn delete-by-index [v row-num]
+  (vec (concat (take row-num v)
+               (drop (inc row-num) v))))
+
+(defn pad [shape place]
+  (vec (concat
+        (take place (repeat []))
+        shape
+        (take (- 10 place (count shape)) (repeat [])))))
+
+(defn land [panel shape place]
   (let [width (count shape)
-        base (mapv @panel (range col (+ col width)))
-        height (find-max-height base shape)]
-    (doseq [i (range width)]
-      (swap! panel update (+ col i) fill (get shape i) height))
-    #_(swap! panel update fill (get shape 0) height)))
+        base (mapv panel (range place (+ place width)))
+        height (find-required-height base shape)
+        padded-shape (pad shape place)]
+    (mapv #(fill %1 %2 height) panel padded-shape)))
 
-(land! t 2)
+#_
+(defn trim [col]
+  (cond (empty? col) []
+        (zero? (last col)) (trim (butlast col))
+        :else (vec col)))
 
-@panel
+#_
+(defn smooth [panel]
+  (mapv trim panel))
+
+(defn disappear
+  ([panel]
+   (disappear panel 0))
+  ([panel row-num]
+   (let [current-row (map #(get % row-num) panel)
+         ended? (every? nil? current-row)
+         made? (every? #(= 1 %) current-row)]
+     (cond ended? panel
+           made? (disappear (map #(delete-by-index % row-num) panel) row-num)
+           :else (disappear panel (inc row-num))))))
+
+(defn measure [panel]
+  (apply max (map count panel)))
+
+(-> (init-panel)
+    (land (:i shapes) 0)
+    (disappear)
+    (land (:i shapes) 4)
+    (disappear)
+    (land (:q shapes) 8)
+    (disappear)
+   #_ (measure))
+;; => 1
+
+
+(-> (init-panel)
+    (land (:t shapes) 1)
+    (disappear)
+    (land (:z shapes) 3)
+    (disappear)
+    (land (:i shapes) 4)
+    (disappear)
+    (measure))
+
+;; => 
+
+
+
+(:i shapes)
+
+
+
+
+
+(land [[] [0 1] [1 1] [0 1] [] [] [] [] [] []]
+      (:z shapes)
+      3)
+
+ 
+
+
+
+
+
+
+
+
+
--- tetris-engine-clj/test/core_test.clj
+++ tetris-engine-clj/test/core_test.clj
@@ -3,9 +3,9 @@
    [core :as src]
    [clojure.test :as t]))
 
-(t/deftest pad-test
-  (t/is (= [1 2 3 nil nil nil nil]
-           (src/pad [1 2 3] 7))))
+(t/deftest cushion-test
+  (t/is (= [1 2 3 0 0 0 0]
+           (src/cushion [1 2 3] 7))))
 
 (t/deftest count-zero-piece
   (t/is (= 0 (src/count-zero-pieces [1 0 1 ])))
@@ -20,19 +20,35 @@
   (t/is (= 0 (src/find-height-single [] [0 1]))))
 
 (t/deftest find-max-height-test
-  (t/is (= 0 (src/find-max-height [[] [] []] [[0 1] [1 1] [0 1]])))
-  (t/is (= 1 (src/find-max-height [[] [1] []] [[0 1] [1 1] [0 1]])))
-  (t/is (= 1 (src/find-max-height [[1 1] [1 1] [1]] [[0 1] [0 1] [0 1]])))
-  (t/is (= 2 (src/find-max-height [[1 1] [1 1] [1]] [[0 1] [1 1] [0 1]]))))
+  (t/is (= 0 (src/find-required-height [[] [] []] [[0 1] [1 1] [0 1]])))
+  (t/is (= 1 (src/find-required-height [[] [1] []] [[0 1] [1 1] [0 1]])))
+  (t/is (= 1 (src/find-required-height [[1 1] [1 1] [1]] [[0 1] [0 1] [0 1]])))
+  (t/is (= 2 (src/find-required-height [[1 1] [1 1] [1]] [[0 1] [1 1] [0 1]]))))
 
 (t/deftest fill-test
-  (t/is (= [1 2 3 nil nil nil nil 8 9] (src/fill [1 2 3] [8 9] 7)))
+  (t/is (= [1 2 3 0 0 0 0 8 9] (src/fill [1 2 3] [8 9] 7)))
+  (t/is (= [1 0 0 0 0 0 0 1 1 1] (src/fill [1] [1 1 1] 7)))
+  (t/is (= [1] (src/fill [1] [] 7))))
 
-  )
+(t/deftest delete-by-index-test
+  (t/is (= [1 2 4 5] (src/delete-by-index [1 2 3 4 5] 2))))
 
-(mapv ["a" "b" "c"] (range 1 3))
+(t/deftest pad-test
+  (t/is (= [[] [] [] [] [] [1 1] [1 1] [] [] []]
+           (src/pad [[1 1] [1 1]] 5))))
 
-2
-0 0
-1 1 
-1 1 1
+(t/deftest land-test
+  (t/is (= [[] [0 1] [1 1] [0 1] [] [] [] [] [] []]
+           (src/land [[] [] [] [] [] [] [] [] [] []] [[0 1] [1 1] [0 1]] 1)))
+  (t/is (= [[] [0 1] [1 1] [0 1 1] [0 1 1 1] [0 1 0 1] [0 0 0 1] [0 0 0 1] [] []] 
+           (src/land [[] [0 1] [1 1] [0 1 1] [0 1 1] [0 1] [] [] [] []] [[1] [1] [1] [1]] 4)))
+  (t/is (= [[] [0 1] [1 1] [0 1 1] [0 1 1] [0 1] [] [] [] []]
+           (src/land [[] [0 1] [1 1] [0 1] [] [] [] [] [] []] [[0 1] [1 1] [1]] 3)))
+  (t/is (= [[] [] [] [] [] [] [] [1] [1 1 1 1] [0 0 1 1]]
+           (src/land [[] [] [] [] [] [] [] [1] [1 1] []] [[1 1] [1 1]] 8)))
+  (t/is (= [[] [] [0 0 0 0 1 1] [1 1 1 1 1 1] [] [] [] [] [] []]
+           (src/land [[] [] [] [1 1 1 1] [] [] [] [] [] []] [[1 1] [1 1]] 2))))
+
+(t/deftest disppear-test
+  (t/is (= [[] [] []] (src/disappear [[1 1 1] [1 1 1] [1 1 1]])))
+  (t/is (= [[0 1] [] [1]] (src/disappear [[0 1 1 1] [0 1 1] [1 1 1]]))))
